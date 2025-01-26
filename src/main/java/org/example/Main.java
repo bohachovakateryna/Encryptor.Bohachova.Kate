1f067c803a48779bff6614ec33d09f658b36e667
Kateryna Bohachova <dak52193@gmail.com>
2025-01-26 16:51:41 +0200

    init commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,13 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+        if(args.length == 2 || args.length == 3){
+            String command = args[0];
+            String filePath = args[1];
+            String key = args[2];
+        } else{
+            Console.beginProgram();
+        }
 
     }
 }
